836266b99386adfe6b7f6ee1fb59f6349885059a
DevYeop <lee.sangyeop0715@gmail.com>
2024-06-10 16:23:52 +0900

    first commit

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+// import App from './App1.vue'
+// import App from './App2.vue';
+import App from './App3.vue';
+
+const pinia = createPinia();
+
+const app = createApp(App);
+
+app.use(pinia);
+app.mount('#app');
